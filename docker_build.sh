080f99b33f7adf3037b64771d02734bfe46ed93c
Remco Perlee <remco.perlee@gmail.com>
2019-03-27 20:15:39 +0100

    Kubernetes 1.14 removed docker

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-microk8s.docker build --no-cache=true -t 127.0.0.1:32000/nginx .
-microk8s.docker push 127.0.0.1:32000/nginx
+docker build --no-cache=true -t 127.0.0.1:32000/nginx .
+docker push 127.0.0.1:32000/nginx
